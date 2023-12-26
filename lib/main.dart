11d8ade66b635cd9bb2c6b07d7b3c9eb510fbffc
adel7ussein <aadelshabib67009@gmail.com>
2023-12-26 22:59:00 +0200

    Implemented dynamic theme switching based on weatherModel  through Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_state.dart';
 import 'package:weather_app/pages/home_page.dart';
 import 'package:weather_app/services/weather_service.dart';
 
@@ -18,17 +19,24 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch:
-            BlocProvider.of<WeatherCubit>(context).weatherModel == null
+    return BlocBuilder<WeatherCubit, WeatherState>(
+      builder: (context, state) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch:
+            BlocProvider
+                .of<WeatherCubit>(context)
+                .weatherModel == null
                 ? Colors.blue
-                : BlocProvider.of<WeatherCubit>(context)
-                    .weatherModel!
-                    .getThemeColor(),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+                : BlocProvider
+                .of<WeatherCubit>(context)
+                .weatherModel!
+                .getThemeColor(),
+          ),
+          debugShowCheckedModeBanner: false,
+          home: HomePage(),
+        );
+      },
     );
   }
 }
